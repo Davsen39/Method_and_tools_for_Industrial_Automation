8b99fd89b1ce8c4ed58c9fcceb72c76e328dfb76
Izanagi95 <32931807+Izanagi95@users.noreply.github.com>
2019-04-26 23:50:07 +0200

    Update project1.m

--- project1.m
+++ project1.m
@@ -1,25 +1,40 @@
 clear;
 clc;
 
-orizzonte=100;
+%% Parametri
+orizzonte=10;
 campionamento=0.1;
 dimensione=(orizzonte/campionamento) +1;
 x=zeros(2,dimensione);
 u=zeros(1,dimensione-1);
 x(:,1)=[0 1]';
-A=[-1 0; 0 1];
-B=[1 0]';
+A=[0 1; -1 0];
+B=[0 1]';
 C=eye(2);
 D=[0 0]';
 sys = ss(A,B,C,D);
 sysd = c2d(sys,campionamento);
+Ad=sysd.A;
+Bd=sysd.B;
 
+%% Stabilità
 autovalori_continuo=eig(A);
-autovalori_discreto=eig(abs(sysd.A));
-
+autovalori_discreto=abs(eig(sysd.A));
+figure(1);
 initial(sysd,x(:,1),orizzonte);
-%tempo=0:campionamento:orizzonte;
-%setpoint=10-ones(1,dimensione);
-%errore=x(1)-setpoint(1);
+
+%% Ottimizzazione
+Q=eye(2);
+Qf=Q;
+R=1;
+[K,S,E] = dlqr(Ad,Bd,Q,R);
+
+%% Stabilità ciclo chiuso
+Ad_chiuso=Ad-Bd*K;
+autovalori_discreto_chiuso=abs(eig(Ad_chiuso));
+sysd_chiuso=ss(Ad_chiuso,Bd,C,D,campionamento);
+figure(2);
+initial(sysd_chiuso,x(:,1),orizzonte);
+
 
 
