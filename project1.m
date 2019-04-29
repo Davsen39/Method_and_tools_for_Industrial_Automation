ec10d97beca7091140a6eb462532afdfb92dc5e4
Izanagi95 <32931807+Izanagi95@users.noreply.github.com>
2019-04-29 11:10:12 +0200

    Update project1.m

--- project1.m
+++ project1.m
@@ -24,9 +24,9 @@ figure(1);
 initial(sysd,x(:,1),orizzonte);
 
 %% Ottimizzazione
-Q=eye(2);
+Q=eye(2)*2;
 Qf=Q;
-R=1;
+R=0.25;
 [K,S,E] = dlqr(Ad,Bd,Q,R);
 
 %% Stabilità ciclo chiuso
@@ -37,4 +37,29 @@ figure(2);
 initial(sysd_chiuso,x(:,1),orizzonte);
 
 
+%% seno
+x = 1:0.1:10;
+y = zeros(1,length(x));
+for i=1:length(x)
+    y(i) = 4*sin(2*x(i)+10);
+end
+
+figure(3);
+plot(x,y);
+
+
+%% grafico a mano
+Y = zeros(2,orizzonte);
+X = zeros(2,orizzonte);
+U = zeros(2,orizzonte - 1);
+X(:,1) = [0 1]';
+
+for i=1:orizzonte-1
+    Y(:,i) = C*X(:,i);
+    X(:,i+1) = A*X(:,i);
+end
+figure(4);
+plot(1:orizzonte,Y(1,:));
+figure(5);
+plot(1:orizzonte,Y(2,:));
 
