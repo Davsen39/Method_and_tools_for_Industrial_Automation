3e04c953b1a752058718e3c11a8d67990ccc46a5
Izanagi95 <32931807+Izanagi95@users.noreply.github.com>
2019-07-06 15:57:09 +0200

    Add files via upload

--- progetto2/respect_constraints.m
+++ progetto2/respect_constraints.m
@@ -1,25 +1,45 @@
 function respect = respect_constraints(a, b, constraints)
 
     next = setdiff(b,a);
+    % controllo i constraints x->valore
     if ismember(next,constraints(:,2))
-        indexes=find(constraints(:,2)==2);
+        indexes=find(constraints(:,2)==next);
+        indexes1=indexes;
         counter=0;
         for i=1:length(indexes)
-            previous=constraints(indexes,1);
+            previous=constraints(indexes(i),1);
             if ismember(previous,a)
                 counter=counter +1;
-            else 
-                break;
             end
         end
 
-    if(counter == length(indexes))
-        respect = 1;
-    else
-        respect = 0;
-    end
+        if(counter == length(indexes))
+            respect = 1;
+        else
+            respect = 0;
+        end
     else 
-        respect = 0;
+        respect = 1;
+    end
+    
+    % controllo i constraints x->valore
+    if respect==1 && ismember(next,constraints(:,1))
+        indexes=find(constraints(:,1)==next);
+        counter=0;
+        for i=1:length(indexes)
+            next_next=constraints(indexes(i),2);
+            if ~ismember(next_next,a)
+                counter=counter +1;
+            end
+        end
+
+        if(counter == length(indexes))
+            respect = 1;
+        else
+            respect = 0;
+        end
+    else 
+        respect = 1;
     end
     
 end
